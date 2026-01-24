4932253f1e8ed191da3c4c94cef3e4ee1cc0c83a
berthold69 <thethien100@gmail.com>
2026-01-24 13:28:21 +0700

    :sparkles: feat: add dashboard & transaction-form components

---
+++ src/app/features/transaction-form/transaction-form.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-form',
+  imports: [],
+  templateUrl: './transaction-form.html',
+  styleUrl: './transaction-form.css',
+})
+export class TransactionForm {
+
+}
