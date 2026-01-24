7746ad1acab057eab6cfdb9b7a08318403fc27b6
berthold69 <thethien100@gmail.com>
2026-01-24 13:37:55 +0700

    :vertical_traffic_light: chore: setup navbar with navigation to dashboard and transaction page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { Dashboard } from './features/dashboard/dashboard';
+import { TransactionForm } from './features/transaction-form/transaction-form';
+export const routes: Routes = [
+    {
+        path: '',
+        component: Dashboard
+    },
+    {
+        path: 'add',
+        component: TransactionForm
+    },
+    {
+        path: 'edit/:id',
+        component: TransactionForm
+    },
+    {
+        path: '**',
+        redirectTo: '' 
+    }, 
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Navbar } from './layout/navbar/navbar';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, Navbar],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
